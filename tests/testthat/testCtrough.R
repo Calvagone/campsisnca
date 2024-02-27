3da9f5d02a10abec7b388ccc340ad2eaab3f0eaa
luyckxn <nicolas.luyckx@calvagone.com>
2024-02-27 14:32:26 +0100

    #17 iValue for Ctrough.

---
+++ tests/testthat/testCtrough.R
@@ -0,0 +1,18 @@
+
+library(testthat)
+library(dplyr)
+library(campsis)
+context("Test Ctrough method")
+
+testFolder <- ""
+source(paste0(testFolder, "testUtils.R"))
+
+test_that("Dataset 1 - Ctrough at steady state", {
+  ds <- dataset1()
+  ctrough1a <- Ctrough(ds$campsis, "Y") %>% campsisnca::calculate()
+  ctrough1b <- Ctrough(ds$campsis, "Y", time=168) %>% campsisnca::calculate()
+  ctrough2 <- ncappcOutput(ds$nonmem, metric="Clast", doseType="ss", doseTime=144, Tau=24)
+  expect_equal(ctrough1a@individual, ctrough2, tolerance=1e-3)
+  expect_equal(ctrough1b@individual, ctrough2, tolerance=1e-3)
+})
+
