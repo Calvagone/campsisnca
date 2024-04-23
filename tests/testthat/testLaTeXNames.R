7f7819ed87ab266c49d664382a38bc575206c18a
luyckxn <nicolas.luyckx@calvagone.com>
2024-04-23 10:33:31 +0200

    #37 Fix test.

--- tests/testthat/testLaTeXNames.R
+++ tests/testthat/testLaTeXNames.R
@@ -4,14 +4,14 @@ library(testthat)
 context("Test method getLaTeXName")
 
 test_that("LaTex names derived from script cat.R are correct", {
-  expect_equal(Ctrough() %>% getLaTeXName(), "C_{trough}")
-  expect_equal(Clast() %>% getLaTeXName(), "C_{last}")
-  expect_equal(CAt() %>% getLaTeXName(), "Conc")
-  expect_equal(Last() %>% getLaTeXName(), "Last value")
-  expect_equal(ValueAt() %>% getLaTeXName(), "Value")
+  expect_equal(Ctrough() %>% campsisnca::getLaTeXName(), "C_{trough}")
+  expect_equal(Clast() %>% campsisnca::getLaTeXName(), "C_{last}")
+  expect_equal(CAt() %>% campsisnca::getLaTeXName(), "Conc")
+  expect_equal(Last() %>% campsisnca::getLaTeXName(), "Last value")
+  expect_equal(ValueAt() %>% campsisnca::getLaTeXName(), "Value")
 })
 
 test_that("LaTex names derived from script cavg.R are correct", {
-  expect_equal(Cavg() %>% getLaTeXName(), "C_{avg}")
-  expect_equal(Avg() %>% getLaTeXName(), "Avg") # No subscript used
+  expect_equal(Cavg() %>% campsisnca::getLaTeXName(), "C_{avg}")
+  expect_equal(Avg() %>% campsisnca::getLaTeXName(), "Avg") # No subscript used
 })
