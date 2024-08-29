1b44cf81bc700cc41809879a4fe984521cbedeb8
luyckxn <nicolas.luyckx@calvagone.com>
2024-08-29 08:39:23 +0200

    #56 Remove level argument.

--- R/generic.R
+++ R/generic.R
@@ -6,19 +6,18 @@
 #' Calculate.
 #' 
 #' @param object object (PK metric) that needs to be calculated
-#' @param level prediction interval level, default is 0.9 (90\% prediction interval)
 #' @param ... extra arguments
 #' @return updated object
 #' @export
 #' @rdname calculate
-calculate <- function(object, level=NULL, ...) {
+calculate <- function(object, ...) {
   stop("No default function is provided")
 }
 
-setGeneric("calculate", function(object, level=NULL, ...) {
-  if (is.null(level)) {
-    level <- 0.9
-  }
+setGeneric("calculate", function(object, ...) {
+  # if (is.null(level)) {
+  #   level <- 0.9
+  # }
   standardGeneric("calculate")
 })
 
