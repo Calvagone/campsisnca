e09a3388127d9687045cb9ca13dbb69cee4e77d6
luyckxn <nicolas.luyckx@calvagone.com>
2024-10-07 14:56:35 +0200

    #62 Deal with special cases.

--- R/time_above_below.R
+++ R/time_above_below.R
@@ -14,22 +14,26 @@ setClass(
   "time_above_below_limit",
   representation(
     limit="numeric",
-    above="logical"
+    above="logical",
+    strictly="logical"
   ),
   contains="nca_metric",
-  validity=validateTmaxMetric
+  validity=validateTimeAboveBelowLimitMetric
 )
 
 #' 
 #' Time above a certain limit.
 #' 
 #' @inheritParams metricsParams
+#' @param limit the limit to compare the variable against
+#' @param strictly whether the variable must be strictly above the limit
 #' @export
-TimeAboveLimit <- function(x=NULL, variable=NULL, limit=NULL, name=NULL, unit=NULL, stat_display=NULL, digits=NULL) {
+TimeAboveLimit <- function(x=NULL, variable=NULL, limit=NULL, strictly=FALSE, name=NULL, unit=NULL, stat_display=NULL, digits=NULL) {
   metric <- ncaConstructor(x=x, variable=variable, name=name, unit=unit, stat_display=stat_display, digits=digits,
                            metric_name="time_above_below_limit", def_name="Timeabove")
   metric@limit <- limit
   metric@above <- TRUE
+  metric@strictly <- strictly
   return(metric)
 }
 
@@ -37,12 +41,15 @@ TimeAboveLimit <- function(x=NULL, variable=NULL, limit=NULL, name=NULL, unit=NU
 #' Time below a certain limit.
 #' 
 #' @inheritParams metricsParams
+#' @param limit the limit to compare the variable against
+#' @param strictly whether the variable must be strictly below the limit
 #' @export
-TimeBelowLimit <- function(x=NULL, variable=NULL, limit=NULL, name=NULL, unit=NULL, stat_display=NULL, digits=NULL) {
+TimeBelowLimit <- function(x=NULL, variable=NULL, limit=NULL, strictly=FALSE, name=NULL, unit=NULL, stat_display=NULL, digits=NULL) {
   metric <- ncaConstructor(x=x, variable=variable, name=name, unit=unit, stat_display=stat_display, digits=digits,
                            metric_name="time_above_below_limit", def_name="Timebelow")
   metric@limit <- limit
   metric@above <- FALSE
+  metric@strictly <- strictly
   return(metric)
 }
 
@@ -54,6 +61,7 @@ TimeBelowLimit <- function(x=NULL, variable=NULL, limit=NULL, name=NULL, unit=NU
 setMethod("iValue", signature=c("time_above_below_limit", "numeric", "numeric"), definition=function(object, time, value) {
   above <- object@above
   value <- value - object@limit
+  strictly <- object@strictly
 
   size <- length(time)
 
@@ -62,37 +70,68 @@ setMethod("iValue", signature=c("time_above_below_limit", "numeric", "numeric"),
     y1=value[1:(size - 1)],
     x2=time[2:size],
     y2=value[2:size],
-    above=rep(above, size - 1)
+    above=rep(above, size - 1),
+    strictly=rep(strictly, size - 1)
     )
   
   durations <- lines %>%
-    purrr::pmap_dbl(computeDuration)
+    purrr::pmap_dbl(computeTimeAboveBelow)
 
   return(sum(durations))    
 })
 
-computeDuration <- function(x1, y1, x2, y2, above) {
+#' 
+#' Compute the duration of a line segment above or below a certain limit.
+#' 
+#' @param x1 x-coordinate of the first point
+#' @param y1 y-coordinate of the first point
+#' @param x2 x-coordinate of the second point
+#' @param y2 y-coordinate of the second point
+#' @param above whether the line segment is above the limit
+#' @param strictly whether the line segment is strictly above or below the limit
+#' @return the duration of the line segment above or below the limit
+#' @export
+computeTimeAboveBelow <- function(x1, y1, x2, y2, above, strictly) {
   duration <- x2 - x1
+  if (duration==0) {
+    stop("Sample times must be unique")
+  }
   
-  if (y1 >= 0 && y2 >= 0) {
+  # Both values are 0
+  if (y1 == 0 && y2 == 0) {
+    return(ifelse(strictly, 0, duration))
+  }
+  
+  # Both values above 0
+  if (y1 > 0 && y2 > 0) {
     return(ifelse(above, duration, 0))
   }
-  if (y1 <= 0 && y2 <= 0) {
+  
+  # Both values below 0
+  if (y1 < 0 && y2 < 0) {
     return(ifelse(!above, duration, 0))
   }
   
+  # Only 1 value is 0
+  if ((y1 == 0 && y2 > 0) || (y1 > 0 && y2 == 0)) {
+    return(ifelse(above, duration, 0))
+  } else if ((y1 == 0 && y2 < 0) || (y1 < 0 && y2 == 0)) {
+    return(ifelse(!above, duration, 0))
+  }
+  
+  # Otherwise, find intersection
   a <- (y2 - y1) / (x2 - x1)
   b <- y1 - a*x1
   intersection <- -b/a
   
   if (intersection > x1 && intersection < x2) {
-    if (y1 >= 0) {
+    if (y1 > 0) {
       return(ifelse(above, intersection - x1, x2 - intersection))
     } else {
       return(ifelse(!above, intersection - x1, x2 - intersection))
     }
   }
-  stop("Should never occur")
+  stop("intersection should lie between x1 or x2")
 }
 
 #_______________________________________________________________________________
--- tests/testthat/testTimeAboveBelow.R
+++ tests/testthat/testTimeAboveBelow.R
@@ -16,7 +16,7 @@ test_that("Dataset 1 - time above 0.75 at day 1", {
   # spaghettiPlot(ind1, "CP") +
   #   ggplot2::geom_hline(yintercept=0.75, linetype="dashed")
   
-  results1 <- TimeAboveLimit(x=campsis, variable="CP", limit=0.75) %>% calculate()
+  results1 <- TimeAboveLimit(x=campsis, variable="CP", limit=0.75) %>% campsisnca::calculate()
   results1@individual
   
   outputRegressionTest(results1@individual, filename="dataset1_time_above_0_75.csv")
@@ -28,9 +28,46 @@ test_that("Dataset 1 - time below 0.75 at day 1 (check reciprocity)", {
   spaghettiPlot(campsis, "CP")
 
   # Make sure results are reciprocal with first test
-  results2 <- TimeBelowLimit(x=campsis, variable="CP", limit=0.75) %>% calculate()
+  results2 <- TimeBelowLimit(x=campsis, variable="CP", limit=0.75) %>% campsisnca::calculate()
   results2@individual$value <- 24 - results2@individual$value
   results2@individual
   
   outputRegressionTest(results2@individual, filename="dataset1_time_above_0_75.csv")
+})
+
+test_that("Method 'computeTimeAboveBelow' deals well with special cases", {
+  
+  # Horizontal line, strictly higher than 0
+  expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=0, above=TRUE, strictly=TRUE), 0)
+  
+  # Horizontal line, higher or equal to 0
+  expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=0, above=TRUE, strictly=FALSE), 3)
+  
+  # Horizontal line, strictly lower than 0
+  expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=0, above=FALSE, strictly=TRUE), 0)
+  
+  # Horizontal line, lower or equal to 0
+  expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=0, above=FALSE, strictly=FALSE), 3)
+  
+  for (strictly in c(FALSE, TRUE)) {
+    # First value is 0, second value is 1
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=1, above=TRUE, strictly=strictly), 3)
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=1, above=FALSE, strictly=strictly), 0)
+    
+    # First value is 0, second value is -1
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=-1, above=TRUE, strictly=strictly), 0)
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=0, y2=-1, above=FALSE, strictly=strictly), 3)
+    
+    # First value is 1, second value is 0
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=1, y2=0, above=TRUE, strictly=strictly), 3)
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=1, y2=0, above=FALSE, strictly=strictly), 0)
+    
+    # First value is -1, second value is 0
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=-1, y2=0, above=TRUE, strictly=strictly), 0)
+    expect_equal(computeTimeAboveBelow(x1=0, x2=3, y1=-1, y2=0, above=FALSE, strictly=strictly), 3)
+  }
+  
+  # Vertical line should raise an error
+  expect_error(computeTimeAboveBelow(x1=0, x2=0, y1=0, y2=1, above=TRUE, strictly=TRUE), msg="Sample times must be unique")
+
 })
