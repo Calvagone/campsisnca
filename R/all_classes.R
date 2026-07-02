cecf29de124290d56f9af98b75451236ff764e37
luyckxn <nicolas.luyckx@calvagone.com>
2026-07-02 18:15:22 +0200

    #83 Define campsis_output.

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,3 +1,5 @@
-setOldClass("tbl_df")
-setOldClass(c("std_campsis_tbl", "tbl_df", "data.frame"))
-setClassUnion("campsis_output", c("data.frame", "tbl_df", "std_campsis_tbl"))
+#' @import tibble
+#' @import campsis
+#' @importClassesFrom campsis campsis_tbl std_campsis_tbl
+#' @export
+setClassUnion("campsis_output", c("data.frame", "tbl_df", "campsis_tbl", "std_campsis_tbl"))
