ac194fca89daa2c2ae3bfbd433376fb9bc02aba9
luyckxn <nicolas.luyckx@calvagone.com>
2026-07-06 11:10:32 +0200

    #84 Support JSON as list.

--- R/json_interface.R
+++ R/json_interface.R
@@ -11,6 +11,9 @@
 #' @keywords internal
 #' 
 openJSON <- function(json, schema=NULL) {
+  if (is.list(json)) {
+    return(JSONElement(json)) # Don't go further if data is already parsed
+  }
   assertthat::assert_that(length(json)==1, msg="Argument json must be a path or the JSON string")
   
   if (grepl(pattern="\\s*\\[", x=json)) {
