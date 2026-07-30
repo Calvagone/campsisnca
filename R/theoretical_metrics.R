de9246215cbba625375bf6fbbd70eab5e9bb87ef
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2026-07-30 12:42:06 +0200

    #88 Do not use global variables

--- R/theoretical_metrics.R
+++ R/theoretical_metrics.R
@@ -19,6 +19,7 @@ rename_original_column <- function(x, from, to) {
 #' @importFrom dplyr all_of group_by select slice ungroup
 #' @importFrom purrr map2
 #' @importFrom campsis obs_only
+#' @importFrom rlang .data
 #' @export
 metrics.common <- function(x, map, thalf.1cpt) {
   x <- x %>% campsis::obs_only()
@@ -39,7 +40,7 @@ metrics.common <- function(x, map, thalf.1cpt) {
   x <- x %>% dplyr::select(dplyr::all_of(c("ID", "TIME", required)))
   
   # Keep first row
-  x <- x %>% dplyr::group_by(ID) %>% dplyr::slice(1) %>% dplyr::ungroup()
+  x <- x %>% dplyr::group_by(.data$ID) %>% dplyr::slice(1) %>% dplyr::ungroup()
   return(x)
 }
 
@@ -50,13 +51,14 @@ metrics.common <- function(x, map, thalf.1cpt) {
 #' @param map character vector used for column mapping, only one key is possible: K
 #' @return theoretical metrics
 #' @importFrom dplyr mutate
+#' @importFrom rlang .data
 #' @export
 metrics.1cpt <- function(x, map=character(0)) {
   # Pre-processing
   x <- x %>% metrics.common(map=map, thalf.1cpt=TRUE)
   
   # Compute
-  x <- x %>% dplyr::mutate(THALF=log(2)/K) # Elimination half life
+  x <- x %>% dplyr::mutate(THALF=log(2)/.data$K) # Elimination half life
   
   return(x)
 }
@@ -68,28 +70,29 @@ metrics.1cpt <- function(x, map=character(0)) {
 #' @param map character vector used for column mapping, keys to be chosen among: DOSE, TAU, CL, V2, Q, V3, KA
 #' @return theoretical metrics
 #' @importFrom dplyr mutate
+#' @importFrom rlang .data
 #' @export
 metrics.2cpt <- function(x, map=character(0)) {
   # Pre-processing
   x <- x %>% metrics.common(map=map, thalf.1cpt=FALSE)
   
   # Compute
-  x <- x %>% dplyr::mutate(K20=CL/V2, K23=Q/V2, K32=Q/V3)
-  x <- x %>% dplyr::mutate(A=1, B=K20+K23+K32, C=K20*K32, DETER=B^2-4*A*C)
-  x <- x %>% dplyr::mutate(ALPHA=(B+sqrt(DETER))/2)
-  x <- x %>% dplyr::mutate(BETA=(B-sqrt(DETER))/2)
-  x <- x %>% dplyr::mutate(COEFF_A=DOSE*KA*(K32-ALPHA)/(V2*(BETA-ALPHA)*(KA-ALPHA)))
-  x <- x %>% dplyr::mutate(COEFF_B=DOSE*KA*(K32-BETA)/(V2*(ALPHA-BETA)*(KA-BETA)))
-  x <- x %>% dplyr::mutate(COEFF_C=DOSE*KA*(K32-KA)/(V2*(ALPHA-KA)*(BETA-KA)))
-  x <- x %>% dplyr::mutate(AUC=DOSE/CL)
-  x <- x %>% dplyr::mutate(AUC_Z_1=COEFF_A/ALPHA)
-  x <- x %>% dplyr::mutate(AUC_Z=COEFF_B/BETA)
-  x <- x %>% dplyr::mutate(AUC_KA=COEFF_C/KA)
-  x <- x %>% dplyr::mutate(AUC_Z_1_pc=AUC_Z_1/(AUC_Z_1+AUC_Z)*100)
-  x <- x %>% dplyr::mutate(AUC_Z_pc=AUC_Z/(AUC_Z_1+AUC_Z)*100)
-  x <- x %>% dplyr::mutate(THALF_D=log(2)/ALPHA) # Distribution half life
-  x <- x %>% dplyr::mutate(THALF_Z=log(2)/BETA)  # Elimination half life
-  x <- x %>% dplyr::mutate(THALF_EFF=THALF_D*AUC_Z_1_pc/100 + THALF_Z*AUC_Z_pc/100) # Effective half life
+  x <- x %>% dplyr::mutate(K20=.data$CL/.data$V2, K23=.data$Q/.data$V2, K32=.data$Q/.data$V3)
+  x <- x %>% dplyr::mutate(A=1, B=.data$K20+.data$K23+.data$K32, C=.data$K20*.data$K32, DETER=.data$B^2-4*.data$A*.data$C)
+  x <- x %>% dplyr::mutate(ALPHA=(.data$B+sqrt(.data$DETER))/2)
+  x <- x %>% dplyr::mutate(BETA=(.data$B-sqrt(.data$DETER))/2)
+  x <- x %>% dplyr::mutate(COEFF_A=.data$DOSE*.data$KA*(.data$K32-.data$ALPHA)/(.data$V2*(.data$BETA-.data$ALPHA)*(.data$KA-.data$ALPHA)))
+  x <- x %>% dplyr::mutate(COEFF_B=.data$DOSE*.data$KA*(.data$K32-.data$BETA)/(.data$V2*(.data$ALPHA-.data$BETA)*(.data$KA-.data$BETA)))
+  x <- x %>% dplyr::mutate(COEFF_C=.data$DOSE*.data$KA*(.data$K32-.data$KA)/(.data$V2*(.data$ALPHA-.data$KA)*(.data$BETA-.data$KA)))
+  x <- x %>% dplyr::mutate(AUC=.data$DOSE/.data$CL)
+  x <- x %>% dplyr::mutate(AUC_Z_1=.data$COEFF_A/.data$ALPHA)
+  x <- x %>% dplyr::mutate(AUC_Z=.data$COEFF_B/.data$BETA)
+  x <- x %>% dplyr::mutate(AUC_KA=.data$COEFF_C/.data$KA)
+  x <- x %>% dplyr::mutate(AUC_Z_1_pc=.data$AUC_Z_1/(.data$AUC_Z_1+.data$AUC_Z)*100)
+  x <- x %>% dplyr::mutate(AUC_Z_pc=.data$AUC_Z/(.data$AUC_Z_1+.data$AUC_Z)*100)
+  x <- x %>% dplyr::mutate(THALF_D=log(2)/.data$ALPHA) # Distribution half life
+  x <- x %>% dplyr::mutate(THALF_Z=log(2)/.data$BETA)  # Elimination half life
+  x <- x %>% dplyr::mutate(THALF_EFF=.data$THALF_D*.data$AUC_Z_1_pc/100 + .data$THALF_Z*.data$AUC_Z_pc/100) # Effective half life
   
   return(x)
 }
